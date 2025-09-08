7d7cc745327a1a2ee0ead795132cdb396d453e23
Daniel Kent <42790722+DannyToast@users.noreply.github.com>
2025-09-08 12:55:21 -0400

    Update camelwebservice.groovy
    Updating for JDK17 (addressing vulnerability)

--- vars/camelwebservice.groovy
+++ vars/camelwebservice.groovy
@@ -12,7 +12,7 @@ def call(Map args = [:]) {
                     spec:
                       containers:
                       - name: maven
-                        image: maven:3.8.1-jdk-11-slim
+                        image: maven:3.8.3-openjdk-17
                         securityContext:
                           runAsUser: 1000
                         env:
